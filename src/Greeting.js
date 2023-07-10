fb31efd9c7da4c0900ba8da7239d57384ac25858
David Espino <davidespinog@gmail.com>
2023-07-10 11:30:19 -0600

    Add redux configuration

---
+++ src/Greeting.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function Greeting() {
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <p>Greeting</p>
+
+    </>
+  );
+}
+
+export default Greeting;
